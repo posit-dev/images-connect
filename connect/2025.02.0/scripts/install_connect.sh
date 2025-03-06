da765fd109445ecc0de3061cc3a1f5360e564dbb
Benjamin R. J. Schwedler <ben@posit.co>
2025-03-06 13:44:19 -0600

    Backfill connect image versions to all in support

---
+++ connect/2025.02.0/scripts/install_connect.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eou pipefail
+
+# Output delimiter
+d="===="
+
+CONNECT_VERSION=${CONNECT_VERSION}
+OS_URL=${OS_URL}
+
+echo "$d Fetching Posit Connect package $d"
+
+# fetch latest deb package
+curl -fsSL "https://cdn.posit.co/connect/$(echo $CONNECT_VERSION | sed -r 's/([0-9]+\.[0-9]+).*/\1/')/rstudio-connect_${CONNECT_VERSION}~${OS_URL}_amd64.deb" -o /tmp/rstudio-connect.deb
+
+echo "$d Verify Posit Connect package $d"
+# Verify the deb package
+pti syspkg install -p dpkg-sig
+gpg --keyserver keys.openpgp.org --recv-keys 51C0B5BB19F92D60
+dpkg-sig --verify /tmp/rstudio-connect.deb
+pti syspkg uninstall -p dpkg-sig
+
+echo "$d Install Posit Connect $d"
+pti syspkg update
+RSTUDIO_INSTALL_NO_LICENSE_INITIALIZATION=1 apt-get install -yf /tmp/rstudio-connect.deb
+
+# clean up
+pti syspkg clean
+rm /tmp/rstudio-connect.deb
