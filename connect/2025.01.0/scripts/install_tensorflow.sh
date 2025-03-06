a7bfc3d1492adc0663be021cfea98239c1a2d74f
Benjamin R. J. Schwedler <ben@posit.co>
2025-03-06 13:43:08 -0600

    Update templates and goss tests in tandem

--- connect/2025.01.0/scripts/install_tensorflow.sh
+++ connect/2025.01.0/scripts/install_tensorflow.sh
@@ -9,11 +9,12 @@ d="===="
 echo "$d Install Tensorflow package $d"
 
 # fetch latest deb package
+pti syspkg update
+pti syspkg install -p curl
 
 curl -fsSL https://storage.googleapis.com/tensorflow-serving-apt/tensorflow-serving.release.pub.gpg -o /usr/share/keyrings/tensorflow-serving.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/tensorflow-serving.gpg] http://storage.googleapis.com/tensorflow-serving-apt stable tensorflow-model-server tensorflow-model-server-universal" | tee /etc/apt/sources.list.d/tensorflow-serving.list
 
-pti syspkg update
 pti syspkg install -p tensorflow-model-server
 
 # clean up
