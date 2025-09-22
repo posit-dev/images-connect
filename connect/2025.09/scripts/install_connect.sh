40df44a1d1dacbaa59b7488f93f55f9ba3a31925
Ian H Pittwood <ipittwood@condata.com>
2025-09-22 11:25:05 -0600

    Add newer versions

---
+++ connect/2025.09/scripts/install_connect.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -eou pipefail
+
+# Output delimiter
+d="===="
+
+apt-get update -yq
+
+echo "$d Installing Posit Connect 2025.09.0 $d"
+
+RSTUDIO_INSTALL_NO_LICENSE_INITIALIZATION=1 apt-get install -yf rstudio-connect=2025.09.0
+apt-mark hold rstudio-connect
+
+mv /tmp/rstudio-connect.gcfg /etc/rstudio-connect/rstudio-connect.gcfg
+if [ "$IMAGE_VARIANT" != "Minimal" ]; then
+cat << EOF >> /etc/rstudio-connect/rstudio-connect.gcfg
+[R]
+Enabled = true
+Executable = /opt/R/4.5.1/bin/R
+
+[Python]
+Enabled = true
+Executable = /opt/python/cpython-3.13.7-linux-x86_64-gnu/bin/python
+
+[Quarto]
+Enabled = true
+Executable = /opt/quarto/1.8.24/bin/quarto
+
+[TensorFlow]
+Enabled = true
+Executable = /usr/bin/tensorflow_model_server
+EOF
+else
+cat << EOF >> /etc/rstudio-connect/rstudio-connect.gcfg
+[Quarto]
+Enabled = false
+
+[TensorFlow]
+Enabled = false
+EOF
+fi
+
+# clean up
+apt-get clean -yqq && \
+rm -rf /var/lib/apt/lists/*
