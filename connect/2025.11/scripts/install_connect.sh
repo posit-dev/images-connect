520de1918b43cd155a1747b83e2f8df9115470df
Ian H. Pittwood <ian.pittwood@posit.co>
2026-04-01 10:24:12 -0600

    Reapply updates

--- connect/2025.11/scripts/install_connect.sh
+++ connect/2025.11/scripts/install_connect.sh
@@ -20,7 +20,7 @@ Executable = /opt/R/4.5.2/bin/R
 
 [Python]
 Enabled = true
-Executable = /opt/python/cpython-3.14.2-linux-x86_64-gnu/bin/python
+Executable = /opt/python/3.14.2/bin/python
 
 [Quarto]
 Enabled = true
